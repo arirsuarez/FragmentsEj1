81e64a85300be78eabf1455940110cb50f5f7940
AriS <arielr.suarez@gmail.com>
2019-09-21 12:18:45 -0300

    Configuro .xml Fragment Rojo y Verde

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.fragments;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonRojo;
+    private Button buttonVerde;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
